77446f713ac4d632407be4c02cacb18dff4051c8
Burhan Yılmaz <wburhany@gmail.com>
2018-07-01 20:44:03 +0300

    Ui kütüphanesi eklendi. .gitignore güncellendi.

--- src/route/route.js
+++ src/route/route.js
@@ -3,6 +3,7 @@ import { Route , BrowserRouter} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from '../redux/store';
 import { Home } from '../modules/Home/index';
+import 'material-components-web/dist/material-components-web.min.css';
 
 export default(
     <Provider store={store}>
